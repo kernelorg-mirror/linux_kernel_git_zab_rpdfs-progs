1f69b94e54cf79c808eb2ae4db17e73d7e47e6a4
Zach Brown <zab@zabbo.net>
2025-03-04 14:18:13 -0800

    Remove multi-threaded devd code
    The processing path of devd is now built around our single threaded
    voluntarily preempting utasks.  It's no longer re-useing the
    multi-threaded client-side block cache nor networking code.
    
    Signed-off-by: Zach Brown <zab@zabbo.net>

--- devd/recv.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef NGNFS_DEVD_RECV_H
-#define NGNFS_DEVD_RECV_H
-
-int devd_recv_setup(struct ngnfs_fs_info *nfi);
-void devd_recv_destroy(struct ngnfs_fs_info *nfi);
-
-#endif

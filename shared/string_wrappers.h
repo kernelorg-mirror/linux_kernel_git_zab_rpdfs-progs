6013d045a09f2331d93161161f5743f7ffa58271
Zach Brown <zab@zabbo.net>
2025-03-10 10:14:04 -0700

    Add a header for string.h helpers
    Are these worth it?  I'm not sure, let's see.
    
    Signed-off-by: Zach Brown <zab@zabbo.net>

---
+++ shared/string_wrappers.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef NGNFS_SHARED_STRING_WRAPPERS_H
+#define NGNFS_SHARED_STRING_WRAPPERS_H
+
+#include <string.h>
+
+#define memset_zero_sizeof(x)		memset(&(x), 0, sizeof(x))
+
+#endif

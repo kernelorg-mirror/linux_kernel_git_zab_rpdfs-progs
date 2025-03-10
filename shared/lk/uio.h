1664300f1988eaa9ae7839584b572788dd684a67
Zach Brown <zab@zabbo.net>
2025-03-10 10:17:34 -0700

    Add bare lk/uio.h
    We're only using iov_length.
    
    Signed-off-by: Zach Brown <zab@zabbo.net>

---
+++ shared/lk/uio.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef NGNFS_SHARED_LK_UIO_H
+#define NGNFS_SHARED_LK_UIO_H
+
+/*
+ * Total number of bytes covered by an iovec.
+ *
+ * NOTE that it is not safe to use this function until all the iovec's
+ * segment lengths have been validated.  Because the individual lengths can
+ * overflow a size_t when added together.
+ */
+static inline size_t iov_length(const struct iovec *iov, unsigned long nr_segs)
+{
+        unsigned long seg;
+        size_t ret = 0;
+
+        for (seg = 0; seg < nr_segs; seg++)
+                ret += iov[seg].iov_len;
+        return ret;
+}
+
+#endif

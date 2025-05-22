4e17a9f56c2731bf5ffc572cd13880debecec5d2
Zach Brown <zab@zabbo.net>
2025-05-22 15:50:32 -0700

    Add simple counted list
    Add a small wrapper around list_head lists that maintain a count.  We
    only have a few operations that are currently used.
    
    Signed-off-by: Zach Brown <zab@zabbo.net>

---
+++ shared/clist.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef NGNFS_SHARED_CLIST_H
+#define NGNFS_SHARED_CLIST_H
+
+/*
+ * I could have sworn that I saw something like this upstream once,
+ * somewhere..
+ */
+struct counted_list_head {
+	struct list_head head;
+	unsigned long count;
+};
+
+static inline void clist_del_init(struct list_head *head, struct counted_list_head *clist)
+{
+	list_del_init(head);
+	clist->count--;
+}
+
+static inline void clist_add_tail(struct list_head *head, struct counted_list_head *clist)
+{
+	list_add_tail(head, &clist->head);
+	clist->count++;
+}
+
+#endif

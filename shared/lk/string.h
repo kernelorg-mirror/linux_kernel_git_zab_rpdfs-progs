5a5968b0c629e7dffa2e3740f09931bdf1d3bb5e
Zach Brown <zab@zabbo.net>
2025-05-31 15:24:13 -0700

    Add lk mem_is_zero
    Add a version of the kernel's mem_is_zero that doesn't use the kernel's
    memchr_inv().  We use the adorable trick of comparing the buffer with
    itself after checking the start.
    
    Signed-off-by: Zach Brown <zab@zabbo.net>

--- shared/lk/string.h
+++ shared/lk/string.h
@@ -4,4 +4,12 @@
 
 #include <string.h>
 
+static inline bool mem_is_zero(const void *s, size_t n)
+{
+	const unsigned char *buf = s;
+
+	/* unaligned, but cute! */
+	return n == 0 || (buf[0] == 0 && (n == 1 || !memcmp(buf, buf + 1, n - 1)));
+}
+
 #endif

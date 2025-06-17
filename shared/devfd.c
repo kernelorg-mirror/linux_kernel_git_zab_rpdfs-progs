17812142102560d99d0e8504f95dae6ef6040d74
Zach Brown <zab@zabbo.net>
2025-06-17 11:49:57 -0700

    Add devfd helpers for working with device files
    Add some small wrappers around the behavioural differences between
    device files that are either block devices or regular files.
    
    Signed-off-by: Zach Brown <zab@zabbo.net>

---
+++ shared/devfd.c
@@ -0,0 +1,90 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#define _GNU_SOURCE /* fallocate() */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <linux/fs.h>
+#include <sys/ioctl.h>
+
+#include "shared/lk/types.h"
+
+#include "shared/devfd.h"
+#include "shared/nerr.h"
+
+/*
+ * Some functions for working with devices as file descriptors in
+ * userspace.  We need slightly different behavior when typically
+ * working with block devices or when less frequently working with
+ * regular files.
+ */
+
+int devfd_get_size(int fd, u64 *size_ret)
+{
+	struct stat st;
+	u64 size = 0;
+	int ret;
+
+	ret = fstat_nerr(fd, &st);
+	if (ret < 0)
+		goto out;
+
+	if (S_ISBLK(st.st_mode)) {
+		if (ioctl(fd, BLKGETSIZE64, &size)) {
+			ret = -errno;
+			goto out;
+		}
+
+	} else if (S_ISREG(st.st_mode)) {
+		size = st.st_size;
+	} else {
+		ret = -EINVAL;
+		goto out;
+	}
+
+	ret = 0;
+out:
+	*size_ret = size;
+	return ret;
+}
+
+int devfd_write_zeros(int fd, u64 start, u64 len)
+{
+	struct stat st;
+	u64 range[2];
+	int ret;
+
+	ret = fstat_nerr(fd, &st);
+	if (ret < 0)
+		goto out;
+
+	if (S_ISBLK(st.st_mode)) {
+		range[0] = start;
+		range[1] = len;
+
+		if (ioctl(fd, BLKDISCARD, range)) {
+			ret = -errno;
+			goto out;
+		}
+
+	} else if (S_ISREG(st.st_mode)) {
+		ret = fallocate(fd, FALLOC_FL_ZERO_RANGE, start, len);
+		if (ret == 0)
+			ret = fsync(fd);
+		if (ret < 0) {
+			ret = -errno;
+			goto out;
+		}
+	} else {
+		ret = -EINVAL;
+		goto out;
+	}
+
+	ret = 0;
+out:
+	return ret;
+}
---
+++ shared/devfd.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef NGNFS_SHARED_DEVFD_H
+#define NGNFS_SHARED_DEVFD_H
+
+#include "shared/lk/types.h"
+
+int devfd_get_size(int fd, u64 *size_ret);
+int devfd_write_zeros(int fd, u64 start, u64 len);
+
+#endif

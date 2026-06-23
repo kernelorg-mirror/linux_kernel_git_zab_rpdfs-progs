ff95c4a3a7a38e657fe4b5fc4e558020fd30f4b4
Zach Brown <zab@zabbo.net>
2026-06-23 15:07:46 -0700

    rpdfs-progs: add memmove_array_tail
    Add a string wrapper for moving the end of an array to make room for
    inserting a region or to collapse a removed region.
    
    Signed-off-by: Zach Brown <zab@zabbo.net>

--- shared/string_wrappers.h
+++ shared/string_wrappers.h
@@ -6,4 +6,18 @@
 
 #define memset_zero_sizeof(x)		memset(&(x), 0, sizeof(x))
 
+/*
+ * Move all the elements from the index to the end of the array by the
+ * distance in units of the index.  The distance can be negative and the
+ * index can be at or past the end of the array.
+ */
+#define memmove_array_tail(ARR, IND, TOTAL, DIST) \
+do { \
+	__typeof__(&(ARR)[0]) arr_ = (ARR); \
+	__typeof__(IND) ind_ = (IND); \
+	__typeof__(TOTAL) total_ = (TOTAL); \
+        if (ind_ < total_) \
+                memmove(&arr_[ind_ + (DIST)], &arr_[ind_], (total_ - ind_) * sizeof(arr_[0])); \
+} while (0)
+
 #endif

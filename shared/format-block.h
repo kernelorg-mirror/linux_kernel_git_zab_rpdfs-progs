3c3a4f08bea3636f08d31c9931bbef49030c0f14
Zach Brown <zab@zabbo.net>
2026-07-10 10:21:17 -0700

    rpdfs-progs: update format-block client changes
    Pull in the changes from the kernel client.
    
    Signed-off-by: Zach Brown <zab@zabbo.net>

--- shared/format-block.h
+++ shared/format-block.h
@@ -13,138 +13,66 @@
 #define RPDFS_BLOCK_SIZE	(1 << RPDFS_BLOCK_SHIFT)
 #define RPDFS_BLOCK_MASK	(RPDFS_BLOCK_SIZE - 1ULL)
 
-struct rpdfs_block_ref {
-	__le64 bnr;
-	__le64 alloc_counter; /* XXX */
-};
+/*
+ * Hand tuned to be a power of two that doesn't limit the number of
+ * reasonably sized dirents/xattrs in a block.
+ *
+ * It might be worth it to squeeze the size of the entry down so we
+ * could go up another power of two.
+ */
+#define RPDFS_EHTABLE_ENTRIES_SHIFT	6
+#define RPDFS_EHTABLE_ENTRIES		(1 << RPDFS_EHTABLE_ENTRIES_SHIFT)
+#define RPDFS_EHTABLE_ENTRIES_MASK	(RPDFS_EHTABLE_ENTRIES - 1)
+
+#define RPDFS_EHTABLE_FULL_ENTRIES	(RPDFS_EHTABLE_ENTRIES * 80 / 100)
+
+/* resolve collisions by assigning low bits, enospc once consumed */
+#define RPDFS_EHTABLE_POS_BITS		2
+#define RPDFS_EHTABLE_POS_MASK		((1UL << RPDFS_EHTABLE_POS_BITS) - 1)
 
 /*
- * The height is one greater than the level of the referenced block.
- * It's 0 for an empty tree.
+ * Each level removes a high bit of the hash and then we want enough
+ * bits remaining for the number of entries in a block.
  */
-struct rpdfs_btree_root {
-	struct rpdfs_block_ref ref;
-	__u8 _pad[7];
-	__u8 height;
+#define RPDFS_EHTABLE_MAX_DEPTH	(32 - RPDFS_EHTABLE_ENTRIES_SHIFT)
+
+struct rpdfs_ehtable_desc {
+	__le32 nr_keys;
+	__le32 total_key_size;
 };
 
-
-/*
- * The first and last keys record the range of item keys that can be
- * found in the block.  It's dependent on (and redundant with) the keys
- * of separating parent items.  Having it in the block makes tracking
- * the range a bit easier to use and to update as we merge and split.
- */
-struct rpdfs_btree_block {
-	__le16 nr_items;
-	__le16 avail_free;
+struct rpdfs_ehtable_block {
+	__le16 tail_free;
 	__le16 total_free;
+	__le16 nr_entries;
+	__u8 depth;
 	__u8 _pad;
-	__u8 level;
-	__le64 items[];
+	struct rpdfs_ehtable_entry {
+		__le32 hash;
+		__le16 offset;
+		__u8 probe_len;
+		__u8 pos;
+	} entries[RPDFS_EHTABLE_ENTRIES];
 };
 
 /*
- * Low bits of the key are used to resolve hash collisions so that an
- * unlucky birthday paradox winner doesn't see errors.
- *
- * We ensure that all the collisions for a key are kept in the same
- * leaf.  This means that we can have to move all the collisions
- * together as we split or merge.  Today the max item size is a
- * worryingly large portion of the block.  We have to keep the number of
- * collision bits very low to avoid the space taken up by collisions
- * exceeding half a block and throwing a bunch of balancing assumptions
- * out the window.
+ * Items are an unaligned payload that starts with the le9 key_size and
+ * u7 val_size which are followed by the bytes of the key and value.
  */
-#define RPDFS_BTREE_KEY_COLL_BITS	1
-#define RPDFS_BTREE_KEY_COLL_MASK	((1ULL << RPDFS_BTREE_KEY_COLL_BITS) - 1)
+struct rpdfs_ehtable_item {
+	__le16 key_val_sizes;
+} __packed;
 
-#define RPDFS_BTREE_VAL_ALIGN_SHIFT	3
-#define RPDFS_BTREE_VAL_ALIGN		(1 << RPDFS_BTREE_VAL_ALIGN_SHIFT)
+#define RPDFS_EHTABLE_KEY_SIZE_FIELD	GENMASK_U16(8, 0)
+#define RPDFS_EHTABLE_VAL_SIZE_FIELD	GENMASK_U16(15, 9)
+#define RPDFS_EHTABLE_MAX_KEY_SIZE	FIELD_MAX(RPDFS_EHTABLE_KEY_SIZE_FIELD)
+#define RPDFS_EHTABLE_MAX_VAL_SIZE	FIELD_MAX(RPDFS_EHTABLE_VAL_SIZE_FIELD)
 
-/*
- * The smaller the max item size, the closer we can come to balancing
- * blocks by used size rather than item count.  This is made a bit worse
- * by keeping all key collisions in the same leaf block.  The max we can
- * be out of balance isn't one item but the number of collisions.  So we
- * push the max size down as far as is reasonable.  The specific max is
- * roughly the size of an xattr with a full compliment of block
- * references.
- */
-#define RPDFS_BTREE_MAX_VAL_SIZE	(8 + 255 + (8 * 16))
-
-/*
- * We pack the fields of each item into a word.  Each shift here is to
- * go from the native form into the packed bit position.  The masks are
- * in terms of the native value.
- *
- * The value off is a bit special because it is aligned and the low bits
- * are always clear.  The shift is to go between the full precision byte
- * offset and the packed offset as a factor of the alignment.  It has
- * masks for both the native and packed form.
- *
- * The key is packed into the most significant bits so that we can shift
- * a search key to match and perform efficient binary searches of words.
- */
-#define RPDFS_BTREE_ITEM_OFF_BITS	(RPDFS_BLOCK_SHIFT - RPDFS_BTREE_VAL_ALIGN_SHIFT)
-#define RPDFS_BTREE_ITEM_SIZE_BITS	order_base_2(RPDFS_BTREE_MAX_VAL_SIZE + 1)
-#define RPDFS_BTREE_ITEM_KEY_BITS	(64 - (RPDFS_BTREE_ITEM_OFF_BITS + \
-					       RPDFS_BTREE_ITEM_SIZE_BITS))
-
-#define RPDFS_BTREE_ITEM_OFF_SHIFT	RPDFS_BTREE_VAL_ALIGN_SHIFT
-#define RPDFS_BTREE_ITEM_OFF_MASK	((1ULL << (RPDFS_BTREE_ITEM_OFF_BITS + \
-						   RPDFS_BTREE_VAL_ALIGN_SHIFT)) - 1)
-#define RPDFS_BTREE_ITEM_OFF_PACK_MASK	((1ULL << RPDFS_BTREE_ITEM_OFF_BITS) - 1)
-#define RPDFS_BTREE_ITEM_SIZE_SHIFT	RPDFS_BTREE_ITEM_OFF_BITS
-#define RPDFS_BTREE_ITEM_SIZE_MASK	((1ULL << RPDFS_BTREE_ITEM_SIZE_BITS) - 1)
-#define RPDFS_BTREE_ITEM_KEY_SHIFT	(RPDFS_BTREE_ITEM_SIZE_SHIFT + \
-					 RPDFS_BTREE_ITEM_SIZE_BITS)
-#define RPDFS_BTREE_ITEM_KEY_MASK	((1ULL << RPDFS_BTREE_ITEM_KEY_BITS) - 1)
-
-/*
- * The inode generation number changes every time a specific inode is
- * freed and reallocated. The inode number and generation number
- * uniquely identify a file/dir over its lifetime. This lets users of
- * references to inodes find out if the inode has changed out from
- * underneath them. Generally, the inode number and generation number
- * should be referenced together, so make a struct to keep them
- * together.
- */
-
-struct rpdfs_ino_gen {
-	__le64 ino;	/* inode number, starts at 1 */
-	__le64 gen;	/* inode generation, starts at 1 */
+struct rpdfs_inode_nr {
+	__le64 i[2];
 };
 
-/*
- * Data blocks are pointed to by a simple tree of indirect blocks rooted
- * in a single field in the inode. The height is one greater than the
- * level of the referenced block. It's 0 for an empty tree.
- */
-struct rpdfs_data_root {
-	struct rpdfs_block_ref ref;
-	__u8 height;
-	__u8 _pad[7];
-};
-
-/*
- * Indirect blocks are a simple array of block refs. We rely on the
- * number of references per indirect block being a power of 2, so check
- * that at compile time.
- */
-#define RPDFS_DATA_REFS_PER_BLK (RPDFS_BLOCK_SIZE / sizeof(struct rpdfs_block_ref))
-
-/*
- * Because blocks per ref is based on the size of a struct, we can't do
- * the smart thing and define the shift first and then the value, we
- * have to go backwards and define the shift from the value instead.
- */
-
-#define RPDFS_DATA_REFS_PER_BLK_SHIFT const_ilog2(RPDFS_DATA_REFS_PER_BLK)
-
-struct rpdfs_indirect_block {
-	struct rpdfs_block_ref refs[RPDFS_DATA_REFS_PER_BLK];
-};
+#define RPDFS_INIT_ROOT_INODE_NR { { cpu_to_le64(1), cpu_to_le64(1) } }
 
 /*
  * Inodes are stored in inode blocks.  Inode blocks numbers are directly
@@ -153,30 +81,22 @@ struct rpdfs_indirect_block {
  * as btree items in the block.
  */
 struct rpdfs_inode {
-	struct rpdfs_ino_gen ig;
+	struct rpdfs_inode_nr ino;
 	__le64 size;
 	__le64 version;			/* changed on file content/metadata changes */
-	struct rpdfs_ino_gen parent_ig;	/* only valid for directories */
 	__le32 nlink;
 	__le32 uid;
 	__le32 gid;
 	__le32 mode;
 	__le32 rdev;
 	__le32 flags;
-	__le32 pad;
-	__le32 xattr_names_len;	/* total length of null-terminated xattr names */
-	__le64 xattr_creates;	/* update on each xattr create and use in key */
 	__le64 atime_nsec;
 	__le64 ctime_nsec;
 	__le64 mtime_nsec;
 	__le64 crtime_nsec;
-	struct rpdfs_btree_root dirents;
-	struct rpdfs_btree_root xattrs;
-	struct rpdfs_data_root data;
+	struct rpdfs_ehtable_desc dirent_eht;
+	struct rpdfs_ehtable_desc xattr_eht;
 };
-
-#define RPDFS_ROOT_INO 1
-#define RPDFS_ROOT_GEN 1
 
 /*
  * This is totally arbitrary.  It looks like it's 32bit in the stat ABI.
@@ -195,13 +115,10 @@ enum rpdfs_dentry_type {
 };
 
 struct rpdfs_dirent {
-	struct rpdfs_ino_gen ig; /* inode number and generation */
+	struct rpdfs_inode_nr ino;
+	__u8 pad[6];
 	__u8 pers_dtype; /* rpdfs persistent directory entry type */
 	__u8 name_len; /* no null termination */
-	union {
-		__u8 pad[6]; /* pad to alignment, stored can be smaller */
-		DECLARE_FLEX_ARRAY(__u8, name);
-	};
 };
 
 /* size of the dirent struct stored in the item before the name */
@@ -211,13 +128,13 @@ struct rpdfs_dirent {
 #define RPDFS_NAME_MAX	255
 
 /* just a random value */
-#define RPDFS_DIRENT_HASH_SEED	0xce94cad8f038f79a
+#define RPDFS_DIRENT_HASH_SEED	0xce94cad8
 
 /* reserved hash values for . and .. */
 #define RPDFS_DIRENT_DOT_HASH	 	0ULL
 #define RPDFS_DIRENT_DOT_DOT_HASH	1ULL
 /* the btree clears collision bits so we must use a min past them */
-#define RPDFS_DIRENT_MIN_HASH		(RPDFS_BTREE_KEY_COLL_MASK + 1)
+#define RPDFS_DIRENT_MIN_HASH		(RPDFS_EHTABLE_POS_MASK + 1)
 
 /*
  * An empty dir contains pseudo entries for "." and "..". The reported
@@ -230,14 +147,8 @@ struct rpdfs_dirent {
 struct rpdfs_xattr {
 	__le16 val_len;
 	__u8 name_len;
-	union {
-		__u8 pad[1]; /* pad to alignment, stored will be bigger */
-		DECLARE_FLEX_ARRAY(__u8, name);
-	};
+	__u8 _pad;
 };
-
-/* size of the xattr struct stored in the item before the name */
-#define RPDFS_XATTR_SIZEOF offsetof(struct rpdfs_xattr, name)
 
 /* max xattr name length, without null term */
 #define	RPDFS_XATTR_MAX_NAME_LEN	RPDFS_NAME_MAX
@@ -248,6 +159,6 @@ struct rpdfs_xattr {
  */
 #define RPDFS_XATTR_MAX_NAMES_LEN	65536
 
-#define RPDFS_XATTR_HASH_SEED		0xfadefadefadefade
+#define RPDFS_XATTR_HASH_SEED		0x416a8d97
 
 #endif
